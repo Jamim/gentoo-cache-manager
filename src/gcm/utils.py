91443a1e57ff2777939c4895511adabb856af92b
Aliaksei Urbanski <aliaksei.urbanski@gmail.com>
2024-05-10 08:43:09 +0300

    ♻️ Use dep_expand for package name normalization

--- src/gcm/utils.py
+++ src/gcm/utils.py
@@ -1,7 +1,7 @@
 import click
 import portage
+from portage.dbapi.dep_expand import dep_expand
 from portage.exception import PackageNotFound
-from portage.versions import pkgsplit
 
 
 def warn(message: str) -> None:
@@ -14,9 +14,10 @@ def pretty_name(name: str) -> str:
 
 def normalize_package_name(name: str) -> str:
     dbapi = portage.db[portage.root]['porttree'].dbapi
-    pacakges = dbapi.match(name)
-    if not pacakges:
+    package: str = dep_expand(name, mydb=dbapi, settings=dbapi.settings).cp
+    if package.startswith('null/') or (
+        '/' in name and not dbapi.cp_list(package)
+    ):
         raise PackageNotFound(name)
 
-    package: str = pkgsplit(pacakges[0])[0]
     return package
