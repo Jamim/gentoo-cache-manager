7bafdc3c10401fbe9505499ede7920d56a3e64c0
Aliaksei Urbanski <aliaksei.urbanski@gmail.com>
2024-05-15 13:59:06 +0300

    ✨ Add the clear command

--- src/gcm/commands/__init__.py
+++ src/gcm/commands/__init__.py
@@ -1,5 +1,11 @@
+from .clear import Clear
 from .disable import Disable
 from .enable import Enable
 from .stats import Stats
 
-COMMANDS = [Enable(), Disable(), Stats()]
+COMMANDS = [
+    Enable(),
+    Disable(),
+    Clear(),
+    Stats(),
+]
